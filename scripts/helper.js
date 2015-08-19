a4982e9251e1dc1eaacc560a15239b32768dab52
sharmanikhil08 <sharma.nikhil@live.com>
2015-08-19 03:36:30 -0300

    Supporting files
    Css, JS etc added

---
+++ scripts/helper.js
@@ -0,0 +1,7 @@
+var skills = '<div class="col-md-3"><img src="./img/%data%.png" alt="My skills" class="img-responsive img-rounded"></div>';
+var skills_array = ['1','2','3','4','5','6','7','8','9','10','11','12'];
+var skills_new = '';
+for(var i = 0; i < skills_array.length(); i += 1){
+	skills_new = skills.replace('%data%', skills_array[i]);
+    $('skills-section-images').append(skills_new);
+}
